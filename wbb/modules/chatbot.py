aacd3e2b939636af3c2760d15b8ee33d00b6f870
hydra-13 <105912211+hydra-13@users.noreply.github.com>
2022-07-17 19:47:58 +0700

    Update chatbot.py

--- wbb/modules/chatbot.py
+++ wbb/modules/chatbot.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 from asyncio import gather, sleep
-
+from googletrans import Translator as google_translator
 from pyrogram import filters
 from pyrogram.types import Message
 
